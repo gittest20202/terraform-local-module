c14539ed741d05248ae56158b36e554605bfa3f2
AMIT CHATURVEDI <65268854+gittest20202@users.noreply.github.com>
2024-01-12 14:33:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,24 @@
-variable "location" {
+variable "rg_location" {
   type = string
-  description = "Region name for the VM"
+  description = "Region location"
+}
+variable "rg_name" {
+   type = string
+   description = "Region name"
+variable  "subscription_id" {
+   type = string
+   description = "Subscription ID"
+}
+variable "tenant_id" {
+   type = string
+   description = "Tenant ID"
+}
+variable "client_id" {
+  type = string
+  description = "Client ID"
+}
+variable "client_secret" {
+   type = string
+   description = "Client Secret"
+   sensitive = true
 }
